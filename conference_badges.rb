aba7d8d108b0db964bc3de1e4a4fce3c11043bba
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 03:28:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,15 @@ def printer(attendees)
     name = badge_maker(name)
     puts name
   end
+<<<<<<< HEAD
   attendees.each.with_index(1) do |name, index|
   puts "Hello, #{name}! You'll be assigned to room #{index}!"
   end
+=======
+  room_assignments = []
+  attendees.each.with_index(1) do |name, index|
+    room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index}!")
+  end
+  puts room_assignments
+>>>>>>> d4dc832a8aa8120af44f8b3a9ba09e92bd74fb8a
 end
